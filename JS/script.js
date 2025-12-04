a8fad1785d6e6d31419541779948d226bdf35a30
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-04 12:35:39 +0100

    JSnack 2

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 /* JSnack 1
- L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore */
+ L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore 
 
  //facciamo inserire all'utente il primo numero
 const numUser1 = parseInt(prompt("Inserisci il primo numero"));
@@ -23,6 +23,36 @@ else{
     numHiger = 'nessuno in quanto sono uguali';
 }
 
-
 //stampiamo il numero maggiore
-console.log('Numero maggiore:', numHiger);
+console.log('Numero maggiore:', numHiger);*/
+
+/*JSnack 2
+ Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+ Il programma stampa la somma di tutti i numeri inseriti. 
+ Esegui questo programma in due versioni, con il for e con il while (facoltativo)*/
+/*
+const numUser1 = parseInt(prompt("Inserisci il primo numero"));
+const numUser2 = parseInt(prompt("Inserisci il secondo numero"));
+const numUser3 = parseInt(prompt("Inserisci il terzo numero"));
+const numUser4 = parseInt(prompt("Inserisci il quarto numero"));
+const numUser5 = parseInt(prompt("Inserisci il quinto numero"));
+
+//metodo semplice con la somma
+let numSomma = numUser1 + numUser2 + numUser3 + numUser4 + numUser5
+console.log(numSomma)*/
+
+const numsUser = [
+  parseInt(prompt("Inserisci il primo numero"), 10),
+  parseInt(prompt("Inserisci il secondo numero"), 10),
+  parseInt(prompt("Inserisci il terzo numero"), 10),
+  parseInt(prompt("Inserisci il quarto numero"), 10),
+  parseInt(prompt("Inserisci il quinto numero"), 10)
+];
+
+let Somma = 0;
+
+for ( let i = 0; i < numsUser.length; i++){
+    Somma += numsUser[i];  
+}
+
+console.log(Somma)
